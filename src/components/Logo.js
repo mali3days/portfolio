2748ca095a85b7d777f31e2d342f05a205b5f215
mali3days <mali3days@gmail.com>
2023-10-23 01:04:56 +0200

    initial commit

---
+++ src/components/Logo.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+  return (
+    <div className="flex items-center justify-center mt-2">
+      <MotionLink
+        href="/"
+        className="flex items-center justify-center w-16 h-16 text-2xl font-bold rounded-full bg-dark text-light"
+        whileHover={{
+          backgroundColor: [
+            '#121212',
+            'rgba(131,58,180,1)',
+            'rgba(253,29,29,1)',
+            'rgba(252,176,69,1)',
+            'rgba(131,58,180,1)',
+            '#121212',
+          ],
+          transition: { duration: 1, repeat: Infinity }
+        }}
+      >
+        MB
+      </MotionLink>
+    </div>
+  );
+};
+
+export default Logo;
